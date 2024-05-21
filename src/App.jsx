5851da70b8460eba80267d39c6fa77e53fa98d65
RickyAnchores <ricky23anchores@gmail.com>
2024-05-21 19:41:26 +0200

    created ads container

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import PokeAPI from './API/PokeApi'
+import Ads from './Components/Ads/Ads'
 
 
 const App = () => {
@@ -10,6 +11,11 @@ const App = () => {
       <div className="container bg-black p-5">
         <h1 className='title text-4xl font-semibold text-center p-4'>PokedeXpedia</h1>
         <PokeAPI />
+        <div className="Ads-container grid grid-cols-1 md:grid-cols-3">
+          <Ads />
+          <Ads />
+          <Ads />
+        </div>
         <div className="credits text-end font-mono p-4">Dev by Ricky 2024</div>
       </div>
     </div>
---
+++ src/Components/Ads/Ads.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Ads = () => {
+  return (
+    <div className='Ads border-2 rounded-lg p-4 m-2'>
+        <img src="" alt="img" />
+        <title>Advertisement</title>
+        <button className="button bg-orange-600 text-white font-bold px-4 hover:animate-pulse">Link</button>
+    </div>
+  )
+}
+
+export default Ads;
