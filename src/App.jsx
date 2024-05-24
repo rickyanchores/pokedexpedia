943132895f7bba307c920c8541a2b5380f61d90f
RickyAnchores <ricky23anchores@gmail.com>
2024-05-24 14:22:18 +0200

    added images and structure for ads

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,34 @@ import Ads from './Components/Ads/Ads'
 
 
 const App = () => {
+
+  const AdCards = [
+    {
+      id: 0,
+      title: "Pokemon OG",
+      image: "https://img.odcdn.com.br/wp-content/uploads/2019/05/20190507084117.jpg"
+    },
+    {
+      id: 1,
+      title: "Pokemon New Series",
+      image: "https://i.ytimg.com/vi/_c_hMehCORQ/hqdefault.jpg"
+    },
+    {
+      id: 2,
+      title: "Pokemon Games",
+      image: "https://cdn.vox-cdn.com/thumbor/KlIlVaMxSyV61YxibUZhaTAcf7k=/0x0:3840x2160/1200x675/filters:focal(1613x773:2227x1387)/cdn.vox-cdn.com/uploads/chorus_image/image/69919398/Pokemon_UNITE___Team_Up._Take_Down.___Screenshot_1.0.png"
+    }
+  ]
+
   return (
     <div className='App min-h-screen flex justify-center items-center'>
       <div className="container bg-black p-5">
         <h1 className='title text-4xl font-semibold text-center p-4'>PokedeXpedia</h1>
         <PokeAPI />
         <div className="Ads-container grid grid-cols-1 md:grid-cols-3">
-          <Ads />
-          <Ads />
-          <Ads />
+          <Ads image={AdCards[0].image}/>
+          <Ads image={AdCards[1].image}/>
+          <Ads image={AdCards[2].image}/>
         </div>
         <div className="credits text-end font-mono p-4">Dev by Ricky 2024</div>
       </div>
--- src/Components/Ads/Ads.jsx
+++ src/Components/Ads/Ads.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Ads = () => {
+const Ads = ({image,title}) => {
   return (
     <div className='Ads border-2 rounded-lg p-4 m-2'>
-        <img src="" alt="img" />
-        <title>Advertisement</title>
+        <img src={image} alt="img" />
+        <title>{title}</title>
         <button className="button bg-orange-600 text-white font-bold px-4 hover:animate-pulse">Link</button>
     </div>
   )
