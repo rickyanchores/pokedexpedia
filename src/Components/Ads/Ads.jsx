6614aced677065da26090299757a832fe0aee2dd
RickyAnchores <ricky23anchores@gmail.com>
2024-05-27 14:25:41 +0200

    applied useMemo and map

--- src/Components/Ads/Ads.jsx
+++ src/Components/Ads/Ads.jsx
@@ -4,7 +4,7 @@ const Ads = ({image,title}) => {
   return (
     <div className='Ads border-2 rounded-lg p-4 m-2'>
         <img src={image} alt="img" />
-        <title>{title}</title>
+        <title className="text-white">{title}</title>
         <button className="button bg-orange-600 text-white font-bold px-4 hover:animate-pulse">Link</button>
     </div>
   )
